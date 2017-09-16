fb9ad442b5a84833f50cc94ef6f4bd2930c39337
David Johns <djohns451@users.noreply.github.com>
2017-09-16 02:36:16 -0400

    First release (1/2)

---
+++ build.sh
@@ -0,0 +1,12 @@
+git clone https://github.com/MicroGSD/RoadArchitect --depth=1
+mv RA/* RoadArchitect/
+xbuild /p:Configuration=Debug
+#mkdir gendarme && cd gendarme
+#wget https://github.com/downloads/spouliot/gendarme/gendarme-2.10-bin.zip
+#unzip gendarme-2.10-bin.zip && rm gendarme-2.10-bin.zip && cd ..
+gendarme --quiet --html ../report.html RoadArchitect/bin/Debug/RoadArchitect.dll
+git checkout gh-pages
+mv ../report.html .
+git add report.html
+git commit -m "Thing for commit #"$TRAVIS_COMMIT
+git push "https://"$GH_TOKEN"@github.com/djohns451/RoadArchitect"
