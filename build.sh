2c276d658c79a8b9117f0de2c4b896f6fa4553a2
David Johns <djohns451@users.noreply.github.com>
2017-09-16 11:37:23 -0400

    First release

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
-git clone https://github.com/MicroGSD/RoadArchitect --depth=1
+git clone https://github.com/MicroGSD/RoadArchitect
 mv RA/* RoadArchitect/
 xbuild /p:Configuration=Debug
-mkdir gendarme && cd gendarme
-wget https://github.com/downloads/spouliot/gendarme/gendarme-2.10-bin.zip
-unzip gendarme-2.10-bin.zip && rm gendarme-2.10-bin.zip && cd ..
-gendarme/gendarme.exe --quiet --html report.html RoadArchitect/bin/Debug/RoadArchitect.dll
-gendarme --quiet --html report.html RoadArchitect/bin/Debug/RoadArchitect.dll
+#mkdir gendarme && cd gendarme
+#wget https://github.com/downloads/spouliot/gendarme/gendarme-2.10-bin.zip
+#unzip gendarme-2.10-bin.zip && rm gendarme-2.10-bin.zip && cd ..
+#gendarme/gendarme.exe --quiet --html report.html RoadArchitect/bin/Debug/RoadArchitect.dll
 cd RoadArchitect
+gendarme --quiet --html ../report.html RoadArchitect/bin/Debug/RoadArchitect.dll
 git checkout gh-pages
 mv ../report.html .
 git add report.html
