7c29bbb1422b8e43b1229785fb6311d03d4f9b11
David Johns <djohns451@users.noreply.github.com>
2017-09-16 03:24:38 -0400

    Commit.

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ mkdir gendarme && cd gendarme
 wget https://github.com/downloads/spouliot/gendarme/gendarme-2.10-bin.zip
 unzip gendarme-2.10-bin.zip && rm gendarme-2.10-bin.zip && cd ..
 gendarme/gendarme.exe --quiet --html report.html RoadArchitect/bin/Debug/RoadArchitect.dll
+gendarme --quiet --html report.html RoadArchitect/bin/Debug/RoadArchitect.dll
 cd RoadArchitect
 git checkout gh-pages
 mv ../report.html .
